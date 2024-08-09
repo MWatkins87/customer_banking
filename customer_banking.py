18755f2af400a60779afd7f89d76405dfbcf6de6
MWatkins87 <matthew.watkins.j@gmail.com>
2024-08-09 13:09:36 -0600

    add: added a utils.py file. added code for cd account. added code for savings account. Fix: fixed issues with customer banking not calling cd or savings account.

--- customer_banking.py
+++ customer_banking.py
@@ -1,5 +1,6 @@
 # Import the create_cd_account and create_savings_account functions
 # ADD YOUR CODE HERE
+from utils import prompt_user
 from cd_account import create_cd_account
 from savings_account import create_savings_account
 # Define the main function
@@ -10,34 +11,34 @@ def main():
     """
     # Prompt the user to set the savings balance, interest rate, and months for the savings account.
     # ADD YOUR CODE HERE
-    savings_balance = float(input('Enter your starting balance: '))
-    savings_interest = float(input('Enter your savings interest rate: '))
-    savings_maturity = int(input('Enter the months: '))
+    savings_balance = prompt_user("Enter your starting balance: ", "float")
+    savings_interest = prompt_user("Enter your savings interest rate: ", "float")
+    savings_maturity = prompt_user("Enter the months: ", "int")
 
     # Call the create_savings_account function and pass the variables from the user.
-    updated_savings_balance = create_savings_account(savings_balance, savings_interest, savings_maturity)
-    new_balance = updated_savings_balance(savings_balance)
+    updated_savings_balance, interest_earned = create_savings_account(savings_balance, savings_interest, savings_maturity)
+    #new_balance = updated_savings_balance(savings_balance)
 
     # Print out the interest earned and updated savings account balance with interest earned for the given months.
     # ADD YOUR CODE HERE
-    print(f"Your new savings balance is $', {new_balance:,.2f}")
-    print(f"Your interest rate is {savings_interest:.2f}")
-    print(f"you earned {updated_savings_balance - savings_balance} over {savings_maturity} months")
+    print(f"Your new savings balance is ${updated_savings_balance:,.2f}")
+    print(f"Your interest rate is {savings_interest:.2f}%")
+    print(f"you earned ${interest_earned:,.2f} over {savings_maturity} months")
 
     # Prompt the user to set the CD balance, interest rate, and months for the CD account.
     # ADD YOUR CODE HERE
-    cd_balance = float(input('Enter your starting balance: '))
-    cd_interest = float(input('Enter your CD interest rate: '))
-    cd_maturity = int(input('Enter the months: '))
+    cd_balance = prompt_user("Enter your starting CD balance:", "float")
+    cd_interest = prompt_user("Enter your CD interest rate:", "float")
+    cd_maturity = prompt_user("Enter the CD months: ", "int")
 
     # Call the create_cd_account function and pass the variables from the user.
-    updated_cd_balance = create_cd_account(cd_balance, cd_interest, cd_maturity)
+    updated_cd_balance, cd_interest_earned = create_cd_account(cd_balance, cd_interest, cd_maturity)
 
     # Print out the interest earned and updated CD account balance with interest earned for the given months.
     # ADD YOUR CODE HERE
-    print(f"Your new CD balance is $', {updated_cd_balance:,.2f}")
-    print(f"Your interest rate is {cd_interest:.2f}")
-    print(f"you earned {updated_cd_balance - cd_balance} over {cd_maturity} months.")
+    print(f"Your new CD balance is ${updated_cd_balance:,.2f}")
+    print(f"Your interest rate is {cd_interest:.2f}%")
+    print(f"you earned ${cd_interest_earned} over {cd_maturity} months.")
 
 if __name__ == "__main__":
     # Call the main function.
---
+++ utils.py
@@ -0,0 +1,40 @@
+def new_interest_balance(account_balance, apr, months):
+    # Calculate interest
+    interest_earned = account_balance * (apr/100 * months/12)
+    # Update the balance
+    account_balance += interest_earned
+    # return the updated balence
+    return account_balance, interest_earned
+
+def is_float(str):
+    return str.replace(".","",1).isdigit()
+
+def prompt_user(prompt, return_type):
+    while True:
+        # Get input
+        user_input = input(prompt)
+        # clean input.
+        user_input = user_input.strip()
+        # make sure its valid
+        match return_type:
+            # for a float
+            case 'float':
+                if is_float(user_input):
+                    user_input = float(user_input)
+                    break
+                else:
+                    print(f"'{user_input}', is not a valid input. Please enter a number.")
+            # for an int
+            case 'int':
+                if user_input.isdigit():
+                    user_input = int(user_input)
+                    break
+                else:
+                    print(f"'{user_input}', is not a valid input. Please enter a number.")
+            # neither float or int exit
+            case _:
+                print(f"'{return_type}' No value entered. Exiting program")
+                exit()
+
+    return user_input
+
